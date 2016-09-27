ff6ac419b9c4a7773a9c899f3de01b3670433492
SgtRumbi <jojos.mobil@gmail.com>
2016-09-27 18:44:39 +0200

    safety first

--- src/lvm/Main.java
+++ src/lvm/Main.java
@@ -3,19 +3,16 @@ package lvm;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Johannes on 27.09.2016.
@@ -25,6 +22,10 @@ public class Main extends Application {
     private int inputMaxBalloonsInPackage = 0;
     private int inputMaxBalloonsInCompartment = 0;
     private int inputCompartmentsCount = 0;
+    private final List<LabelArrayReference> labelArrayReferences = new ArrayList<>();
+    private HBox labelsBox;
+    private Label labelOutput;
+    private Queue<Integer> fillSequenceQueue = new ArrayDeque<>();
 
     // CLASS METHODS
     private GridPane createInputContainer(String labelText, final TextField textField) {
@@ -52,37 +53,179 @@ public class Main extends Application {
         return textField;
     }
 
-    private Parent createItem() {
+    private Label createItem() {
         Label sampleItem = new Label("2");
-        sampleItem.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, new CornerRadii(12), new BorderWidths(6))));
+        // sampleItem.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, new CornerRadii(12), new BorderWidths(6))));
         sampleItem.setBackground(new Background(new BackgroundFill(new Color(0.8, 0.8, 0.8, 1.0), new CornerRadii(12), new Insets(2))));
-        sampleItem.setPadding(new Insets(20));
+        sampleItem.setPadding(new Insets(10, 10, 20, 10));
         return sampleItem;
+    }
+
+    final class LabelArrayReference {
+        private Label label;
+        private boolean clearing = false;
+        private int index, value;
+
+        public LabelArrayReference(Label label, int index, int value) {
+            this.label = label;
+            this.index = index;
+            this.value = value;
+
+            label.setText(index + ": " + value);
+        }
+
+        public void update(int newValue) {
+            this.value = newValue;
+            label.setText(index + ": " + value);
+        }
+
+        public void setClearing(boolean clearing) {
+            this.clearing = clearing;
+            if (clearing) {
+                // label.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, new CornerRadii(12), new BorderWidths(3))));
+                label.setBackground(new Background(new BackgroundFill(new Color(0.7, 0.7, 0.9, 1.0), new CornerRadii(12), new Insets(2))));
+            } else {
+                // label.setBorder(null);
+                label.setBackground(new Background(new BackgroundFill(new Color(0.8, 0.8, 0.8, 1.0), new CornerRadii(12), new Insets(2))));
+            }
+        }
+
+        public boolean isClearing() {
+            return clearing;
+        }
+
+        public Label getLabel() {
+            return label;
+        }
+    }
+
+    private void showLabels() {
+        labelsBox.getChildren().clear();
+        for (LabelArrayReference lar : labelArrayReferences) {
+            labelsBox.getChildren().add(lar.getLabel());
+        }
+    }
+
+    private void setOutput(LVMResult result) {
+        String add = "";
+        LVMLastMatchItem[] fields = result.lastMatch;
+        int value = 0;
+        for (int i = 0; i < fields.length; i++) {
+            value += fields[i].value;
+            add += "" + fields[i].value;
+            if (i != fields.length - 1)
+                add += " + ";
+        }
+        labelOutput.setText("Output: " + add + " = " + value + ", Überschuss: " + result.finalTolerance);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TextField textFieldMaxBalloonsInPackage = createInputField("z. B. 20");
-        TextField textFieldMaxBalloonsInCompartment = createInputField("z. B. 15");
-        TextField textFieldMaxCompartments = createInputField("z. B. 10");
+        final TextField textFieldMaxBalloonsInPackage = createInputField("z. B. 20");
+        final TextField textFieldMaxBalloonsInCompartment = createInputField("z. B. 15");
+        final TextField textFieldMaxCompartments = createInputField("z. B. 10");
 
-        Button buttonFillRandom = new Button("Fülle");
+        labelOutput = new Label("Output: -");
+        labelOutput.setBackground(new Background(new BackgroundFill(new Color(0.9, 0.7, 0.7, 1.0), new CornerRadii(12), new Insets(2))));
+        labelOutput.setPadding(new Insets(10));
+
+        // final int[][] randomArray = {null};
+
+        // Button buttonFillRandom = new Button("Fülle");
         Button buttonCalculate = new Button("Sortiere");
+        buttonCalculate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                int maxBalloonsInPackage = Integer.valueOf(textFieldMaxBalloonsInPackage.getText());
+                int compartmentsCount = Integer.valueOf(textFieldMaxCompartments.getText());
+                if (fillSequenceQueue.size() != 0) {
+                    System.out.println("Deque size: " + fillSequenceQueue.size());
 
-        ToolBar buttonContainer = new ToolBar(buttonFillRandom, buttonCalculate);
+                    // Get the array from the deque
+                    int[] array;
+                    if (fillSequenceQueue.size() >= compartmentsCount) {
+                        array = new int[compartmentsCount];
+                    } else {
+                        array = new int[fillSequenceQueue.size()];
+                    }
+
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = fillSequenceQueue.poll();
+                    }
+
+                    echoArray(array);
+
+                    // Calculate result
+                    LVMResult result = LVM.lvmCalculate(array, maxBalloonsInPackage);
+
+                    // Mark all labels to non clear
+                    for (LabelArrayReference lar : labelArrayReferences) {
+                        lar.setClearing(false);
+                    }
+
+                    // Mark labels to clear
+                    for (int i = 0; i < result.lastMatch.length; i++) {
+                        LVMLastMatchItem item = result.lastMatch[i];
+                        labelArrayReferences.get(item.key).setClearing(true);
+                    }
+
+                    // Set output
+                    setOutput(result);
+                } else System.out.println("Random array is null...");
+            }
+        });
+
+        Label labelFillSequenceDescription = new Label("Füllfolge: ");
+        Label labelFillSequence = new Label("-");
+        Button updateFillSequence = new Button("Anpassen");
+        Button randomFillSequence = new Button("Zufällig");
+        randomFillSequence.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                int maxBalloonsInCompartment = Integer.valueOf(textFieldMaxBalloonsInCompartment.getText());
+                // int maxCompartments = Integer.valueOf(textFieldMaxCompartments.getText());
+
+                System.out.println("Adding random to deque. size: " + fillSequenceQueue.size());
+
+                Random random = new Random();
+                int randomValue = random.nextInt(maxBalloonsInCompartment);
+                fillSequenceQueue.add(randomValue);
+                labelArrayReferences.add(new LabelArrayReference(createItem(), fillSequenceQueue.size(), randomValue));
+
+                System.out.println("Adding random to deque. size: " + fillSequenceQueue.size());
+
+                /* randomArray[0] = generateRandomFilledArray(maxCompartments, maxBalloonsInCompartment);
+                labelArrayReferences.clear();
+                for (int i = 0; i < randomArray[0].length; i++) {
+                    labelArrayReferences.add(new LabelArrayReference(createItem(), i, randomArray[0][i]));
+                } */
+                showLabels();
+            }
+        });
+
+        ToolBar buttonContainer = new ToolBar(buttonCalculate, new Separator(), labelFillSequenceDescription, labelFillSequence, updateFillSequence, randomFillSequence);
         buttonContainer.setPadding(new Insets(5));
         // buttonContainer.setSpacing(5);
 
-        HBox hBoxCompartments = new HBox(createItem(), createItem());
+        /* LabelArrayReference lar = new LabelArrayReference(createItem(), 0, 1);
+        lar.setClearing(true);
+        HBox hBoxCompartments = new HBox(createItem(), createItem(), lar.getLabel());
         hBoxCompartments.setPadding(new Insets(10));
-        hBoxCompartments.setSpacing(10);
+        hBoxCompartments.setSpacing(10); */
+
+        labelsBox = new HBox();
+        ScrollPane labelsBoxScrollPane = new ScrollPane(labelsBox);
+        labelsBoxScrollPane.hbarPolicyProperty().set(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        labelsBoxScrollPane.vbarPolicyProperty().set(ScrollPane.ScrollBarPolicy.NEVER);
 
         VBox root = new VBox(
                 createInputContainer("Max. Luftballons in einer Packung", textFieldMaxBalloonsInPackage),
                 createInputContainer("Max. Luftballons in einem Fach", textFieldMaxBalloonsInCompartment),
                 createInputContainer("Anzahl der Fächer", textFieldMaxCompartments),
                 buttonContainer,
-                hBoxCompartments
+                labelsBoxScrollPane,
+                labelOutput
+                // hBoxCompartments
         );
 
         Scene lvmScene = new Scene(root, 600, 300);
